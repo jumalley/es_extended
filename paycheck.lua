54fce716bb2aa339303e1aeffa0932c548586dd6
Ju <jumalleydev@gmail.com>
2024-10-05 03:01:57 +0200

    Update paycheck.lua

--- paycheck.lua
+++ paycheck.lua
@@ -4,34 +4,22 @@ local function getMultiplierAndIcon(player)
 
     local groupLabel = "User"
     local multiplier = 1
-    local icon = "piggy-bank"
-    local iconColor = "#006400"
 
     if group == "vip" then
         multiplier = 1.5
-        icon = "star"
         groupLabel = "VIP Gold"
-        iconColor = "#FFD700"
     elseif group == "vip2" then
         multiplier = 2
-        icon = "star"
         groupLabel = "VIP Diamond"
-        iconColor = "#b9f2ff"
     elseif group == "moderator" then
         multiplier = 2.5
         groupLabel = "Moderator"
-        icon = "shield-halved"
-        iconColor = "#A020F0"
     elseif group == "admin" then
         multiplier = 3
         groupLabel = "Administrator"
-        icon = "shield-halved"
-        iconColor = "#FF7F7F"
     elseif group == "dev" then
         multiplier = 5
         groupLabel = "Developer"
-        icon = "laptop-code"
-        iconColor = "white"
     end
 
     return multiplier, icon, iconColor, groupLabel
